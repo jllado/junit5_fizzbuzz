beccf857e534fcfce6f32dec04f82df94b65cfba
Juan Lladó <jllado@gmail.com>
2016-08-28 21:15:46 +0200

    Red: FizzBuzz play game test

--- src/test/java/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/kata/fizzbuzz/FizzBuzzTest.java
@@ -1,14 +1,13 @@
 package kata.fizzbuzz;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.*;
 
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
 
 
@@ -41,6 +40,27 @@ public class FizzBuzzTest {
     @Test
     public void return_fizzbuzz_when_number_is_divisible_by_three_and_five() throws Exception {
         assertThat(fizzbuzz(15), is("FizzBuzz"));
+    }
+
+    @Nested
+    @DisplayName("play game")
+    class FizzBuzzGameTest {
+
+        @Test
+        @DisplayName("return from one to one hundred fizzbuzz numbers")
+        public void playGame() throws Exception {
+            assertThat(play(), hasSize(100));
+            assertThat(play().get(0), is("1"));
+            assertThat(play().get(3), is("Fizz"));
+            assertThat(play().get(5), is("Buzz"));
+            assertThat(play().get(16), is("17"));
+            assertThat(play().get(19), is("Buzz"));
+        }
+
+        public List<String> play() {
+            return null;
+        }
+
     }
 
     private String fizzbuzz(Integer number) {
