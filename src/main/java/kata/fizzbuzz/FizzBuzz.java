16a66a1f633d83b9f2daeb7b9e6b51d4ec2463a7
Juan Lladó <jllado@gmail.com>
2016-08-29 23:17:55 +0200

    Refactor: extract FizzBuzz rule

--- src/main/java/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -1,12 +1,23 @@
 package kata.fizzbuzz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class FizzBuzz {
+
+    private List<Rule> rules = new ArrayList<>();
+
     public FizzBuzz() {
+        rules.add(number -> isDivisible(number, 3) && isDivisible(number, 5) ? Optional.of("FizzBuzz") : Optional.empty());
     }
 
     public String of(Integer number) {
-        if (isDivisible(number, 3) && isDivisible(number, 5)) {
-            return "FizzBuzz";
+        for (Rule rule : rules) {
+            Optional<String> result = rule.apply(number);
+            if (result.isPresent()) {
+                return result.get();
+            }
         }
         if (isDivisible(number, 3)) {
             return "Fizz";
---
+++ src/main/java/kata/fizzbuzz/Rule.java
@@ -0,0 +1,12 @@
+package kata.fizzbuzz;
+
+import java.util.Optional;
+
+/**
+ * Created by jllado on 29/08/16.
+ */
+@FunctionalInterface
+public interface Rule {
+    public Optional<String> apply(Integer number);
+
+}
