6bae61dc9f27a5f9143bf305f0e4b61a06684e33
Juan Lladó <jllado@gmail.com>
2016-08-29 23:20:27 +0200

    Refactor: extract Buzz rule

--- src/main/java/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -11,6 +11,7 @@ public class FizzBuzz {
     public FizzBuzz() {
         rules.add(number -> isDivisible(number, 3) && isDivisible(number, 5) ? Optional.of("FizzBuzz") : Optional.empty());
         rules.add(number -> isDivisible(number, 3) ? Optional.of("Fizz") : Optional.empty());
+        rules.add(number -> isDivisible(number, 5) ? Optional.of("Buzz") : Optional.empty());
     }
 
     public String of(Integer number) {
@@ -19,9 +20,6 @@ public class FizzBuzz {
             if (result.isPresent()) {
                 return result.get();
             }
-        }
-        if (isDivisible(number, 5)) {
-            return "Buzz";
         }
         return number.toString();
     }
