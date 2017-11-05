3e445ab18ec6aa6c87e43e69d0ce90d5862a3d83
Juan Lladó <jllado@gmail.com>
2017-11-06 00:52:55 +0100

    New Java 9 sintax to create a List

--- src/main/java/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/FizzBuzz.java
@@ -1,28 +1,23 @@
 package kata.fizzbuzz;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
 
-    private List<Rule> rules = rules();
+    private static final List<Rule> RULES = List.of(
+            new Rule(number1 -> number1.isDivisibleBy(3, 5), "FizzBuzz"),
+            new Rule(number1 -> number1.isDivisibleBy(3), "Fizz"),
+            new Rule(number1 -> number1.isDivisibleBy(5), "Buzz"));
+
     private Number number;
 
     public FizzBuzz(Integer number) {
         this.number = new Number(number);
     }
 
-    private List<Rule> rules() {
-        List<Rule> rules = new ArrayList<>();
-        rules.add(new Rule(number -> number.isDivisibleBy(3, 5), "FizzBuzz"));
-        rules.add(new Rule(number -> number.isDivisibleBy(3), "Fizz"));
-        rules.add(new Rule(number -> number.isDivisibleBy(5), "Buzz"));
-        return rules;
-    }
-
     @Override
     public String toString() {
-        return rules.stream().flatMap(rule -> rule.applyFor(number).stream()).findFirst().orElse(number.toString());
+        return RULES.stream().flatMap(rule -> rule.applyFor(number).stream()).findFirst().orElse(number.toString());
     }
 
 }
